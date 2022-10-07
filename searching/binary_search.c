2852cddf6537eaa0ab849a6c1f15de4adbcbe3ab
Rachit <bhojanirachit@gmail.com>
2022-10-07 12:36:59 +0530

    add binary search code

---
+++ searching/binary_search.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Binarysearch(int a[],int n, int x)
+{
+    int l=0,h=n-1,m;
+    while(l<=h)
+    {
+        m=(l+h)/2;
+        if(a[m]==x)
+        return m;
+        else if(a[m]<x)
+        l=m+1;
+        else
+        h=m-1;
+    }
+    return -1;
+}
+
+void main()
+{
+    int i,n,a[100],x;
+    printf("Enter a no.");
+    scanf("%d",&n);
+    printf("Enter a array\n");
+    for(i=0;i<n;i++)
+    {
+        printf("Enter %d element",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("Enter element to be searched");
+    scanf("%d",&x);
+
+    i=Binarysearch(a,n,x);
+    if(i==-1)
+    {
+        printf("Number not found");
+    }
+    else
+    {
+        printf("Number found at %d index",i);
+    }
+}
